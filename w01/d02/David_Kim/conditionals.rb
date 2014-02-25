16c243601c983610f45dcf734893691aed4af742
davidakimbo <minheeman@gmail.com>
2014-02-25 18:06:19 -0500

    other stuff

---
+++ w01/d02/David_Kim/conditionals.rb
@@ -0,0 +1,14 @@
+# conditionals
+
+puts "What is your name?"
+
+name = gets.chomp
+name = name.downcase
+
+if name == "hari"
+  puts "You're awesome dude!"
+else
+  puts "Oh sorry, you're not hari, so I'm not sure if you're cool."
+end
+
+puts ("That's the end of our conversation")
---
+++ w01/d02/David_Kim/madlib_starter.rb
@@ -0,0 +1,45 @@
+puts "Welcome to the Mad Lib Generator!! Prepare to have some laughs"
+
+puts "Enter a handheld object:"
+handheld_object = gets.chomp
+
+puts "Enter a cartoon character:"
+cartoon_character = gets.chomp
+
+puts "Enter a verb:"
+verb = gets.chomp
+
+puts "Enter a valuable object:"
+valuable_object = gets.chomp
+
+puts "Enter a loud sound:"
+loud_sound = gets.chomp
+
+puts "Enter your life-long enemy:"
+enemy = gets.chomp
+
+puts "Enter an adjective:"
+adjective = gets.chomp
+
+puts "Enter a verb:"
+verb = gets.chomp
+
+ puts "Enter a "
+ = gets.chomp
+
+ puts "Enter a "
+ = gets.chomp
+
+ puts "Enter a "
+ = gets.chomp
+
+
+# Prompt the user to enter different words
+
+
+# Substitute the blank spaces in the template with the words
+
+
+# Print out the results to the user
+puts template
+
