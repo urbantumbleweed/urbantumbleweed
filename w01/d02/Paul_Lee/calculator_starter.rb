89b609f1293c9c2bc1f6f65d095c2e7e9e98ed4b
pjwl33 <paul.leejw@gmail.com>
2014-02-25 18:05:55 -0500

    subtract function added

--- w01/d02/Paul_Lee/calculator_starter.rb
+++ w01/d02/Paul_Lee/calculator_starter.rb
@@ -1,5 +1,4 @@
 puts "What are you doing with the calculator? Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply"
-
 calc_function = gets.chomp.downcase
 
 puts "First number?"
@@ -9,7 +8,12 @@ puts "Second number?"
 second_num = gets.chomp.to_i
 
 add = first_num + second_num
+subtract = first_num - second_num
+
 
 if (calc_function == "a")
   puts "#{first_num} + #{second_num} = #{add}"
+elsif (calc_function == "s")
+  puts "#{first_num} - #{second_num} = #{subtract}"
+
 end
