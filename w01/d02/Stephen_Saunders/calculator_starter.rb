002ca24fcf38195ffaacad8828a542054880f790
urbantumbleweeed <saunders.stephen@gmail.com>
2014-02-25 18:19:05 -0500

    Moved code inside the initializer while loop and created thesubtraction function.

--- w01/d02/Stephen_Saunders/calculator_starter.rb
+++ w01/d02/Stephen_Saunders/calculator_starter.rb
@@ -12,29 +12,39 @@ initializer = gets.chomp
 # Print out a statement with the result. 
 
 
-
-
-# Declare the variables used in the program to collect the input for 
-# any operation.
-num1 = 0
-num2 = 0
-
-#Phase 1 - create the condition that adds
-if initializer == "a"
-	print "Great, you want to add.  What's the first number? "
-	num1 = gets.chomp.to_i
-	
-	print "What's the second number? "
-	num2 = gets.chomp.to_i
-
-	sum = num1 + num2
-
-	puts "Okay, #{num1} plus #{num2} is #{sum}."
-end
-
-
 while initializer != "q"
 	print "Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply"
+
+		# Declare the variables used in the program to collect the input for 
+		# any operation.
+		num1 = 0
+		num2 = 0
+
+		#Phase 1 - create the condition that adds
+		if initializer == "a"
+			print "Great, you want to add.  What's the first number? "
+			num1 = gets.chomp.to_i
+			
+			print "What's the second number? "
+			num2 = gets.chomp.to_i
+
+			sum = num1 + num2
+
+			puts "Okay, #{num1} plus #{num2} is #{sum}."
+
+		# Phase 2 - create the condition and steps to subtract.
+		elsif initializer == "s"
+			print "I can help you with subtraction.  What number do you want to subtract from? "
+			num1 = gets.chomp
+
+			print "OK, now tell me the second number."
+			num2 = gets.chomp
+
+			difference = num1 - num2
+
+			puts "You get #{difference} when you subtract #{num2} from #{num1}."
+
+		end
 
 
 	initializer = gets.chomp
