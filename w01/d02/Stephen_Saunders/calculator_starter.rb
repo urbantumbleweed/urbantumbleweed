f67b8722f68b1262d51c084ae520a80c27a99f29
urbantumbleweeed <saunders.stephen@gmail.com>
2014-02-25 18:06:11 -0500

    added the addition feature and initialize prompt.

---
+++ w01/d02/Stephen_Saunders/calculator_starter.rb
@@ -0,0 +1,41 @@
+initializer = "start"
+
+# Prompt the user to select which operation to do:
+puts "I can help you with math.  What you like to do?"
+puts "add (a), subtract (s), divide (d), or multiply (m)"
+puts "> "
+
+initializer = gets.chomp
+# Prompt the user to enter a math operation
+# Prompt the user to enter two values
+# Perform the proper math operation on the two values entered
+# Print out a statement with the result. 
+
+
+
+
+# Declare the variables used in the program to collect the input for 
+# any operation.
+num1 = 0
+num2 = 0
+
+#Phase 1 - create the condition that adds
+if initializer == "a"
+	print "Great, you want to add.  What's the first number? "
+	num1 = gets.chomp.to_i
+	
+	print "What's the second number? "
+	num2 = gets.chomp.to_i
+
+	sum = num1 + num2
+
+	puts "Okay, #{num1} plus #{num2} is #{sum}."
+end
+
+
+while initializer != "q"
+	print "Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply"
+
+
+	initializer = gets.chomp
+end
