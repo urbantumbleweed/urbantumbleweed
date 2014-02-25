50580b693fda38d5cd79295f3696d8ca3288c78f
jingalli <james@endorse.me>
2014-02-25 18:03:53 -0500

    Phase 1

--- w01/d02/ASSIGNMENT_FILES/calculator_starter.rb
+++ w01/d02/ASSIGNMENT_FILES/calculator_starter.rb
@@ -1,12 +1,23 @@
-initializer = "start"
-
 # Prompt the user to enter a math operation
 # Prompt the user to enter two values
 # Perform the proper math operation on the two values entered
-# Print out a statement with the result. 
-while initializer != "q"
-	print "Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply"
+# Print out a statement with the result.
 
+puts "Enter a math operation (+,-,/,*)!"
 
-	initializer = gets.chomp
+op = gets.chomp
+
+puts "Enter first value!"
+
+x = gets.chomp.to_i
+
+puts "Enter second value!"
+
+y = gets.chomp.to_i
+
+if op == "+"
+  sum = x + y
+  puts "Your result is #{sum}!"
 end
+
+
