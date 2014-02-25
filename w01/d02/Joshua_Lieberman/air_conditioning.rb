3e4a3cfeb24efd7d89080dceb6c69a6ff40d0e52
jlieb10 <joshuathomaslieberman@gmail.com>
2014-02-25 17:08:22 -0500

    Updates before pull

--- w01/d02/Joshua_Lieberman/air_conditioning.rb
+++ w01/d02/Joshua_Lieberman/air_conditioning.rb
@@ -7,12 +7,24 @@ ac = gets.chomp.downcase
 puts "What temperature would you like it to be?"
 des_temp = gets.chomp.to_i
 
-if ((ac == "yes") && (cur_temp > des_temp)) then
+# if ((ac == "yes") && (cur_temp > des_temp))
+# 	puts "Turn on the A/C please!"
+# elsif ((ac == "no") && (cur_temp > des_temp))
+# 	puts "Fix the A/C now! It's hot!"
+# elsif ((ac == "no") && (cur_temp < des_temp))
+# 	puts "Fix the A/C whenever you have the chance.. It's cool.."
+# else
+# 	puts "I don't know how to make it warmer!"
+# end
+
+too_hot = (cur_temp > des_temp)
+
+if (too_hot && ac == "yes")
 	puts "Turn on the A/C please!"
-elsif ((ac == "no") && (cur_temp > des_temp)) then
+elsif (too_hot && ac == "no")
 	puts "Fix the A/C now! It's hot!"
-elsif ((ac == "no") && (cur_temp < des_temp)) then
+elsif (!too_hot && ac == "no")
 	puts "Fix the A/C whenever you have the chance.. It's cool.."
 else
-	puts "I don't know how to make it warmer!"
+	puts "We'll be fine!"
 end
