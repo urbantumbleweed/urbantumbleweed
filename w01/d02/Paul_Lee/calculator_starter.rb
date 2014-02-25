0917e4da05d06f6b7f8864fdfe94a8ea6ad5722e
pjwl33 <paul.leejw@gmail.com>
2014-02-25 18:09:39 -0500

    multiple function added

--- w01/d02/Paul_Lee/calculator_starter.rb
+++ w01/d02/Paul_Lee/calculator_starter.rb
@@ -9,11 +9,15 @@ second_num = gets.chomp.to_i
 
 add = first_num + second_num
 subtract = first_num - second_num
+multiply = first_num * second_num
 
 
 if (calc_function == "a")
   puts "#{first_num} + #{second_num} = #{add}"
 elsif (calc_function == "s")
   puts "#{first_num} - #{second_num} = #{subtract}"
+elsif (calc_function == "m")
+  puts "#{first_num} * #{second_num} = #{multiply}"
+
 
 end
