db12f1c43ca4bf56465e8000323808fc44b67d26
jlieb10 <joshuathomaslieberman@gmail.com>
2014-02-25 15:48:56 -0500

    adding bouncer.rb and conditionals.rb

---
+++ w01/d02/Joshua_Lieberman/conditionals.rb
@@ -0,0 +1,10 @@
+# Conditionals
+
+puts "What is your name?"
+name = gets.chomp.downcase
+if name == "hari"
+	puts "you're awesome, dude"
+else
+	puts "you're not Hari, I'm not sure."
+end
+puts "that's the end of the conversation"
