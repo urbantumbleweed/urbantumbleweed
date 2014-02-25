a65d6654d8ce9d01e55111f708c630559cc859b2
jemise111 <jesse.sessler@gmail.com>
2014-02-25 17:23:06 -0500

    add ruby practice programs from day 2

---
+++ w01/d02/Jesse_Sessler/conditionals.rb
@@ -0,0 +1,15 @@
+# Conditionals
+
+puts 'What is your name?'
+
+name = gets.chomp
+
+name = name.downcase
+
+if name == 'hari'
+  puts("your're awesome, dude!")
+else
+  puts("oh sorry, you're not hari, so I'm not sure if you're awesome")
+end
+
+puts ("That's the end of our conversation")
