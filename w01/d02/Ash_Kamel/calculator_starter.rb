13f85048b75be0bf65c493ce6382acbd25dab122
Ash Kamel <ajkamel@gmail.com>
2014-02-25 18:19:02 -0500

    Add division operator

--- w01/d02/Ash_Kamel/calculator_starter.rb
+++ w01/d02/Ash_Kamel/calculator_starter.rb
@@ -19,7 +19,8 @@ while initializer != "q"
 		puts num1 + num2
 	elsif operator == "s"
 		puts num1 - num2
-	
+	elsif operator == "d"
+		puts num1 / num2
 	else
 		puts "You didnt enter a valid operator or number"
 	end
