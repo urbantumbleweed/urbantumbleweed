42820e6310db7685c1c358569065b1907e58d05e
originalblend337 <originalblend337@gmail.com>
2014-02-25 17:21:08 -0500

    before starting hw for w01d02

---
+++ w01/d02/Tadashi_Okazaki/conditionals.rb
@@ -0,0 +1,8 @@
+puts "Can I have your name?"
+name = gets.chomp.downcase
+
+if name == "hari"
+  puts "You're awesome dude!"
+else
+  puts "Nooooo! You're not Hari!!"
+end
---
+++ w01/d02/Tadashi_Okazaki/my_second_ruby_app.rb
@@ -0,0 +1,3 @@
+puts "Can you tell me your name?"
+name = gets.strip
+puts "Hello #{name}!"
