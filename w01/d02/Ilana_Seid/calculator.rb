8d547253bde1ea66a1d9dee53c2b29069771aaa2
ilanaseid <ilanaseid@gmail.com>
2014-02-25 18:22:42 -0500

    addition

---
+++ w01/d02/Ilana_Seid/calculator.rb
@@ -0,0 +1,12 @@
+puts "What kind of operation would you like to do? (+, -, *, /)?"
+operator = gets.chomp
+
+puts "Input value 1"
+value_1 = gets.chomp.to_f
+
+puts "Input value 2"
+value_2 = gets.chomp.to_f
+
+if operator == "+"
+	puts value_1 + value_2
+end
