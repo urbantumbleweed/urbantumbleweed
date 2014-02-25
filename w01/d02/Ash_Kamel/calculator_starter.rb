567ea8ca193d59c9a8598c6c02daaa0f5b1c5376
Ash Kamel <ajkamel@gmail.com>
2014-02-25 18:11:55 -0500

    Create calculator file with addition

---
+++ w01/d02/Ash_Kamel/calculator_starter.rb
@@ -0,0 +1,32 @@
+initializer = "start"
+
+
+
+# Prompt the user to enter a math operation
+# Prompt the user to enter two values
+# Perform the proper math operation on the two values entered
+# Print out a statement with the result. 
+while initializer != "q"
+	puts "Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply."
+
+	operator = gets.chomp
+
+	puts "Enter first value"
+	num1 = gets.chomp.to_f
+
+	puts "Enter second value"
+	num2 = gets.chomp.to_f
+
+	if operator == "a"
+	puts num1 + num2
+	
+	end
+
+	puts "Press q to finish or enter to calculate a new value"
+
+	initializer = gets.chomp
+
+end
+
+
+
