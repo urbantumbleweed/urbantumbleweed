5f461bd4c19124f655268bfe0ba3d4d641f7fb5e
urbantumbleweeed <saunders.stephen@gmail.com>
2014-02-25 19:24:28 -0500

    Completed Phase 2, added variables to the template.

--- w01/d02/Stephen_Saunders/madlib.rb
+++ w01/d02/Stephen_Saunders/madlib.rb
@@ -20,11 +20,11 @@ adjective = gets.chomp
 puts "Can you give me a NAME?"
 name = gets.chomp
 
-puts "OK, I just need a PROPER NOUN:"
-proper_noun = gets.chomp.capitalize
+puts "OK, I just need a name for a TYPE OF WORKER:"
+type_of_worker = gets.chomp.capitalize
 
 # Substitute the blank spaces in the template with the words
-
+template = "Never #{verb} a bear before you go to bed. I did last night and all night there were #{plural_noun} floating around in my head, dancing around like little #{color} Pokemon. One Pokemon, two Pokemon, #{number} Pokemons. All I could think was 'Will I ever be the very best?' I guess I could be as #{adjective} as the greats that came before me. ASH! MISTY! #{name} ! But alas, only time will tell if I will ever become an amazing #{type_of_worker}. But for now, I'll just try to learn Ruby."
 
 # Print out the results to the user
 puts template
