6fefc65d06f63c9fa7627b7cd1ba38c76084d50c
pjwl33 <paul.leejw@gmail.com>
2014-02-26 15:43:20 -0500

    day3 work

--- w01/d03/Paul_Lee/Classwork/methods_classwork.rb
+++ w01/d03/Paul_Lee/Classwork/methods_classwork.rb
@@ -3,7 +3,8 @@
 def transmogrifier (num_1, num_2,  num_3)
   result = (num_1 + num_2) ** num_3
   return result
+  puts (result)
 end
 
 transmogrified_result = transmogrifier(2, 5, 3)
-puts transmogrified_result
+# puts transmogrified_result
