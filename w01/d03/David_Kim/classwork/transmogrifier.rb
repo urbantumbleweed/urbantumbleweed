a0284a47dbd9c1196e009edc0a39238be04b3928
davidakimbo <minheeman@gmail.com>
2014-02-26 16:16:29 -0500

    my stuffs

--- w01/d03/David_Kim/classwork/transmogrifier.rb
+++ w01/d03/David_Kim/classwork/transmogrifier.rb
@@ -2,7 +2,7 @@ def transmogrifier(num1, num2, num3)
   result = (num1 + num2) ** num3
   puts result
   return result #to store the variable for potential future use. Also ends the method
-end
+end #Put an end if there's no return or multiple returns. end is important for Ruby syntax
 
 a = 1
 b = 3
