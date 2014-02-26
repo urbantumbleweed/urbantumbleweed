5d7d9f61210aba53ce74bdd67709e51d8a0a662d
jlieb10 <joshuathomaslieberman@gmail.com>
2014-02-25 22:50:16 -0500

    Homework end of day

--- w01/d02/Joshua_Lieberman/Homework/madlib_complete.rb
+++ w01/d02/Joshua_Lieberman/Homework/madlib_complete.rb
@@ -2,7 +2,7 @@
 puts "Welcome to the Mad Lib Generator"
 
 while true
-	puts "Would you like to try Game 1 or 2? (Enter "1" or "2")"
+	puts "Would you like to try Game 1 or 2? (Enter '1' or '2')"
 	game_choice = gets.chomp
 	if game_choice == "1"
 
@@ -28,6 +28,32 @@ while true
 		# Print out the results to the user
 		puts template
 	elsif game_choice == "2"
+		puts "Enter a proper noun:"
+		noun1 = gets.chomp.capitalize
+		puts "Enter a noun:"
+		noun2 = gets.chomp.downcase
+		puts "Enter a noun:"
+		noun3 = gets.chomp.downcase
+		puts "Enter a job title:"
+		job = gets.chomp.capitalize
+		puts "Enter a verb:"
+		verb1 = gets.chomp.downcase
+		puts "Enter a place:"
+		place = gets.chomp.downcase
+		puts "Enter a verb (past participle - ending with ‘ed’):"
+		verb2 = gets.chomp.downcase
+		puts "Enter a noun:"
+		noun4 = gets.chomp.downcase
+		puts "Enter a verb (gerund - ending with ‘ing’):"
+		verb3 = gets.chomp.downcase
+		puts "Enter a plural noun:"
+		noun5 = gets.chomp.downcase
+		puts "Enter a noun:"
+		noun6 = gets.chomp.downcase
+		puts "Enter an emotion:"
+		emotion = gets.chomp.downcase
+
+		template = "It was during the Battle of #{noun1} when I was running through a #{noun2} when a #{noun3} went off right next to my platoon. Our #{job} yelled for us to #{verb1} to the nearest #{place} we could find. When we got to the we #{verb2} to start a fire. As we were starting the fire the enemy saw the #{noun4} from the fire and started #{verb3} {noun5} at us. we all quickly ducked behind the #{noun6} at the and returned fire. we quickly eliminated the enemy and were #{emotion} that we had won the battle."
 
 	else
 		puts "Please enter a valid selection"
