f063710597c81cb7b64a2c264b67d175bb7b3bbd
dan987 <dan987@aol.com>
2014-02-26 09:18:12 -0500

    commit 100

---
+++ w01/d03/Dan_Morena/classwork/morning_ex_2.rb
@@ -0,0 +1,21 @@
+puts "base"
+base_n = gets.chomp.to_i
+
+puts "second"
+second_n = gets.chomp.to_i
+
+counter = 0
+second_n.times do
+ counter = counter + 1
+
+ if counter == 0
+  result = base_n + second_n
+  result = result1
+else
+  result = result1 + second_n
+end
+
+puts result
+end
+
+
