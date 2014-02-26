d2948d995c10439bcc6f19580ab51c4d6fa7ad24
davidakimbo <minheeman@gmail.com>
2014-02-25 21:27:18 -0500

    Finished works, including my mad libs

--- w01/d02/David_Kim/madlib_starter.rb
+++ w01/d02/David_Kim/madlib_starter.rb
@@ -16,7 +16,7 @@ puts "Enter a loud sound:"
 loud_sound = gets.chomp
 
 puts "Enter your life-long enemy:"
-enemy = gets.chomp
+enemy = gets.chomp.capitalize
 
 puts "Enter an adjective:"
 adjective = gets.chomp
@@ -24,21 +24,18 @@ adjective = gets.chomp
 puts "Enter a verb:"
 verb = gets.chomp
 
- puts "Enter a "
- = gets.chomp
+puts "Enter an animal"
+animal = gets.chomp
 
- puts "Enter a "
- = gets.chomp
+puts "Enter an adjective"
+adjective = gets.chomp
 
- puts "Enter a "
- = gets.chomp
-
-
-# Prompt the user to enter different words
+puts "Enter a food"
+food = gets.chomp
 
 
 # Substitute the blank spaces in the template with the words
-
+template = "'WHERE IS IT?!'' You scream as you hold a #{handheld_object} to #{cartoon_character}'s head. You #{verb} #{cartoon_character} across the face as you continue your interrogation.'I was supposed to sell the #{valuable_object} and buy puppies with the money!'. You raise your pimp hand right when you heard a loud #{loud_sound} and your life long enemy, #{enemy}, emerges from the shadows!'#{enemy}! What a jerkface! You smell bad and you're ugly!' you shout as you stink-eyed your enemy's #{adjective} face. #{enemy} grabs #{cartoon_character} and jumps on a flying #{animal} with frickin' lasers on it's head. You wake up right when you get blasted, filled with righteous fury. 'What a #{adjective} dream.. I really shouldn't eat #{food} before going to bed!"
 
 # Print out the results to the user
 puts template
