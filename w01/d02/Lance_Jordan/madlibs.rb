5bd4b42edcf0b4935b8f726e075b2cb8ad302bde
lacostenycoder <djlancejordan@gmail.com>
2014-02-26 01:24:26 -0500

    added polite exit message and cleaned un-needed puts

--- w01/d02/Lance_Jordan/madlibs.rb
+++ w01/d02/Lance_Jordan/madlibs.rb
@@ -19,15 +19,16 @@ my_array = Array.new
 array.each do|a|
   puts "Enter a #{a.upcase}"
   a = gets.chomp.upcase
-  puts "#{a} = #{a}" #printing as we go :)
+  #puts "#{a} = #{a}" #printing as we go :)
   my_array << "#{a}" # add new my_array item
-  puts my_array # testing items being added to my_array
+  #puts my_array # testing items being added to my_array
 end
 
 # create madlib with words substituded from my_array
 madlib = "Never -#{my_array[0]}- a bear before you go to bed. I did last night and all night there were -#{my_array[1]}- floating around in my head, dancing around like little -#{my_array[2]}- Pokemon. One Pokemon, two Pokemon, -#{my_array[3]}- Pokemons. All I could think was 'Will I ever be the very best?' I guess I could be as -#{my_array[4]}- as the greats that came before me. ASH! MISTY! -#{my_array[5]}- ! But alas, only time will tell if I will ever become an amazing -#{my_array[6]}-. But for now, I'll just try to learn Ruby."
 
 puts madlib  # run the madlib... hooray!
+puts "Thanks for playing MadLibs!  Goodbye! quitting :)" # politeness :)
 
 #original madlip template below-->
 # Never -VERB- a bear before you go to bed. I did last night and all night there were -NOUN (PLURAL)- floating around in my head, dancing around like little -COLOR- Pokemon. One Pokemon, two Pokemon, -NUMBER- Pokemons. All I could think was 'Will I ever be the very best?' I guess I could be as -ADJECTIVE- as the greats that came before me. ASH! MISTY! -NAME- ! But alas, only time will tell if I will ever become an amazing -PROPER NOUN-. But for now, I'll just try to learn Ruby.
