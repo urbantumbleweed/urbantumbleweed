56b908d920241565e7ad831747bcd037eed31de4
alexshook <alexandra.shook@gmail.com>
2014-02-25 22:36:23 -0500

    partial refactor of calculator

--- w01/d02/Alexandra_Shook/calculator_starter.rb
+++ w01/d02/Alexandra_Shook/calculator_starter.rb
@@ -1,11 +1,9 @@
+# refactor of homework, not submitted
+initializer = "start"
+
 # Prompt the user to enter a math operation
 puts "Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply, (e) for exponent."
-initializer = gets.chomp
-
-while initializer != "a" && initializer != "s" && initializer != "d" && initializer != "m" && initializer != "e"
-	puts "Sorry, I don't understand. Please press (a) to add, (s) to subtract, (d) to divide, (m) to multiply, (e) for exponent."
-  initializer = gets.chomp
-end
+operation = gets.chomp
 
 # Prompt the user to enter two values
 puts "Please enter your first value."
@@ -16,14 +14,20 @@ y = gets.chomp.to_i
 
 # Perform the proper math operation on the two values entered
 # Print out a statement with the result.
-if initializer == "a"
+if operation == "a"
   puts "The answer is " + (x + y).to_s + "."
-elsif initializer == "s"
+elsif operation == "s"
   puts "The answer is " + (x - y).to_s + "."
-elsif initializer == "d"
+elsif operation == "d"
   puts "The answer is " + (x.to_f / y.to_f).to_s + "."
-elsif initializer == "m"
+elsif operation == "m"
   puts "The answer is " + (x * y).to_s + "."
-elsif initializer == "e"
+elsif operation == "e"
   puts "The answer is " + ( x ** y).to_s + "."
+end
+
+# ask how this loop works
+while initializer != "q"
+  puts "Please press (a) to add, (s) to subtract, (d) to divide, (m) to multiply, (e) for exponent, (q) to quit."
+  initializer = gets.chomp
 end
