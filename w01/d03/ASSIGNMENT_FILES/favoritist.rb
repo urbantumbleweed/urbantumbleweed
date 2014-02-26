8629df973ae5d154399d888efdc44cb91e56e990
harimohanraj89 <hari.mohanraj89@gmail.com>
2014-02-26 15:42:17 -0500

    Added favoritist exercise

---
+++ w01/d03/ASSIGNMENT_FILES/favoritist.rb
@@ -0,0 +1,17 @@
+# =========================================
+#               FAVORITIST
+# =========================================
+
+puts("Please enter your favorite food:")
+fav_food = gets.chomp
+
+puts("Please enter your favorite color:")
+fav_color = gets.chomp
+
+puts("Please enter your favorite movie:")
+fav_movie = gets.chomp
+
+puts("Please enter your favorite song:")
+fav_song = gets.chomp
+
+puts("That's awesome! I love #{fav_food}, #{fav_food}, #{fav_food}, and #{fav_food}, too!")
