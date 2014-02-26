14d9911df48baf0dbecae1fab44060ebd6c31143
jingalli <james@endorse.me>
2014-02-25 20:28:08 -0500

    MadLib hw

--- w01/d02/ASSIGNMENT_FILES/madlib_starter.rb
+++ w01/d02/ASSIGNMENT_FILES/madlib_starter.rb
@@ -1,12 +1,33 @@
 
 puts "Welcome to the Mad Lib Generator"
 
-# Prompt the user to enter different words
+puts "Enter a verb!"
 
+verb = gets.chomp
 
-# Substitute the blank spaces in the template with the words
+puts "Enter a plural noun!"sublruby
 
+nouns = gets.chomp
 
-# Print out the results to the user
-puts template
+puts "Enter a color!"
+
+color = gets.chomp
+
+puts "Enter a number!"
+
+num = gets.chomp
+
+puts "Enter an adjective!"
+
+adj = gets.chomp
+
+puts "Enter a name!"
+
+name = gets.chomp
+
+puts "Enter a proper noun!"
+
+prop = gets.chomp
+
+puts "Never #{verb} a bear before you go to bed. I did last night and all night there were #{nouns} floating around in my head, dancing around like little #{color} Pokemon. One Pokemon, two Pokemon, #{num} Pokemons. All I could think was 'Will I ever be the very best?' I guess I could be as #{adj} as the greats that came before me. ASH! MISTY! #{name} ! But alas, only time will tell if I will ever become an amazing #{prop}. But for now, I'll just try to learn Ruby."
 
