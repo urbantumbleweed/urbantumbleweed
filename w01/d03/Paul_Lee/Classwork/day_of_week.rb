c8844cd6bf696584ab62f876ffe52f9635b25533
pjwl33 <paul.leejw@gmail.com>
2014-02-26 13:24:51 -0500

    day of week exercise

--- w01/d03/Paul_Lee/Classwork/day_of_week.rb
+++ w01/d03/Paul_Lee/Classwork/day_of_week.rb
@@ -5,7 +5,7 @@ weekend = [dow_arr.shift, dow_arr.pop]
 
 puts "Weekends are #{weekend}"
 
-puts "Our weekends are gone! #{dow_arr}..sad face"
+puts "Our weekends are gone! #{dow_arr}...sad face"
 
 dow_arr.push("Sat")
 dow_arr.unshift("Sun")
@@ -15,12 +15,14 @@ puts "Our week is back to normal: #{dow_arr}"
 puts "Pick a number between 1-7"
 user_input = gets.chomp.to_i
 
-if user_input < dow_arr.length
+if user_input <= 7
   dow_arr[user_input]
-  puts "Day #{user_input} of the week is #{dow_arr[user_input]}!"
+  puts "Day #{user_input} of the week is #{dow_arr[user_input -= 1]}!"
 
 else
   dow_arr.sample
-  puts "Day #{user_input} of the week is #{dow_arr.sample}!"
+  puts "Day #{dow_arr.sample.to_i} of the week is #{dow_arr.sample}!"
 
 end
+
+# while dow_arr.include? user_input
