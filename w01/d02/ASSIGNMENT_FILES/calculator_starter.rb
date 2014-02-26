91b712284af0adee9c7b0fa0c89033b27182a35c
jingalli <james@endorse.me>
2014-02-25 23:23:45 -0500

    Calculator Assignment - Final Submission

--- w01/d02/ASSIGNMENT_FILES/calculator_starter.rb
+++ w01/d02/ASSIGNMENT_FILES/calculator_starter.rb
@@ -1,39 +1,33 @@
-# Prompt the user to enter a math operation
-# Prompt the user to enter two values
-# Perform the proper math operation on the two values entered
-# Print out a statement with the result.
+initializer = "start"
+
+while initializer != "q"
 
 puts "Enter a math operation (+,-,/,*)!"
-
 op = gets.chomp
 
 puts "Enter first value!"
-
 x = gets.chomp.to_i
 
 puts "Enter second value!"
-
 y = gets.chomp.to_i
 
 if op == "+"
   sum = x + y
   puts "Your result is #{sum}!"
-end
-
-if op == "-"
+elsif op == "-"
   diff = x - y
   puts "Your result is #{diff}!"
-end
-
-if op == "*"
+elsif op == "*"
   product = x * y
   puts "Your result is #{product}!"
-end
-
-if op == "/"
+elsif op == "/"
   divide = x / y
   puts "Your result is #{divide}!"
 end
 
+puts "Type q to quit, enter to keep going!"
 
+initializer = gets.chomp
+
+end
 
