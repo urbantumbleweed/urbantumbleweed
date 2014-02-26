3c247b11c7f0e800e7240717d79aa580c5790612
davidmiller11 <davidmiller11@gmail.com>
2014-02-25 19:42:44 -0500

    last phase with trig functions. #nerdalert

--- w01/d02/Dave_Miller/calculator_app.rb
+++ w01/d02/Dave_Miller/calculator_app.rb
@@ -1,3 +1,5 @@
+#Dave Miller
+
 initializer = "start"
 
 # Prompt the user to enter a math operation
@@ -5,7 +7,7 @@ initializer = "start"
 # Perform the proper math operation on the two values entered
 # Print out a statement with the result.
 while initializer != "q"
-  print "Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply, (e) to use exponents: "
+  print "Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply, (e) to use exponents, (t) to use trig: "
   initializer = gets.chomp
 
   def getNumbers
@@ -16,6 +18,11 @@ while initializer != "q"
     y = gets.chomp.to_f
 
     return [ x , y ]
+  end
+
+  def getTrigNumber
+    puts "Please enter a value: "
+    return gets.chomp.to_f
   end
 
   def add( x , y )
@@ -57,30 +64,25 @@ while initializer != "q"
     when "q"
       puts "You pressed 'q' to quit.  Smell ya later."
       exit
+    when "t"
+      puts "You chose to use trig.  Press (s) for sin, (c) for cos, and (t) for tan: "
+      trig_input = gets.chomp
+      trig_num = getTrigNumber
+      case trig_input
+        when "s"
+          # use sin
+          puts "You chose to use sin. sin(#{trig_num}) = #{Math.sin( trig_num )}."
+        when "c"
+          # use cos
+          puts "You chose to use cos. cos(#{trig_num}) = #{Math.cos( trig_num )}."
+        when "t"
+          # use tangent
+          puts "You chose to use tan. tan(#{trig_num}) = #{Math.tan( trig_num )}."
+        else
+          puts "Not a valid command."
+      end
     else
       puts "Please enter a valid command, or 'q' to quit."
   end
 
 end
-
-
-
-
-
-#####Phase 1
-# The calculator can add two numbers together and return the result.
-
-#####Phase 2
-# The calculator can subtract two numbers and return the result.
-
-#####Phase 3
-#* The calculator can multiply two numbers and return the result.
-
-#####Phase 4
-#* The calculator can divide two numbers and return the result.
-
-
-#####Phase 5 (Bonus)
-#* Add more functionality to your calculator
- # * Try adding the ability to calculate exponents
-  #* Try adding the ability to perform Trigonometric functions (sin, cos, tan)
