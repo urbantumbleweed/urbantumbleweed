8eda51344151d3f221e048d945abe24988657886
verner-dz <verner.dz@gmail.com>
2014-02-25 20:28:27 -0500

    homework and classwork files day 2

---
+++ w01/d02/Verner_DSouza/classroom_work/conditionals.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+name = name.downcase
+if name == "hari"
+  puts "you're awesome"
+else
+  puts "You're not awesome"
+end
+
+puts "this is the end of the conversation."
---
+++ w01/d02/Verner_DSouza/classroom_work/my_third_ruby_app.rb
@@ -0,0 +1,9 @@
+puts "What's your name?"
+name = gets.chomp
+
+puts "And what's your name?"
+second_name = gets.chomp
+
+
+puts "Hey, #{name}, meet #{second_name}!"
+puts "Hey #{second_name}, meet #{name}"
