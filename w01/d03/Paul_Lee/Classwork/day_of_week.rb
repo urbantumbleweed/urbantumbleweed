476e16968a2b70e23cd957d8da18ae2c16cdd69b
pjwl33 <paul.leejw@gmail.com>
2014-02-26 15:27:55 -0500

    classwork at 3:30

--- w01/d03/Paul_Lee/Classwork/day_of_week.rb
+++ w01/d03/Paul_Lee/Classwork/day_of_week.rb
@@ -1,28 +1,29 @@
-#days of week exercise
+#days_arr of week exercise
 
-dow_arr = ["Sun", "Mon", "Tue", "Wed"," Thu", "Fri", "Sat"]
-weekend = [dow_arr.shift, dow_arr.pop]
+days_arr = ["Sun", "Mon", "Tue", "Wed"," Thu", "Fri", "Sat"]
+weekend = [days_arr.shift, days_arr.pop]
 
 puts "Weekends are #{weekend}"
 
-puts "Our weekends are gone! #{dow_arr}...sad face"
+puts "Our weekends are gone! #{days_arr}...sad face"
 
-dow_arr.push("Sat")
-dow_arr.unshift("Sun")
+days_arr.unshift(weekend[0])
+days_arr.push(weekend[1])
 
-puts "Our week is back to normal: #{dow_arr}"
+puts "Our week is back to normal: #{days_arr}"
 
 puts "Pick a number between 1-7"
 user_input = gets.chomp.to_i
 
-if user_input <= 7
-  dow_arr[user_input]
-  puts "Day #{user_input} of the week is #{dow_arr[user_input -= 1]}!"
+random_day = days_arr.sample
+random_index = days_arr.index(random_day)
 
+if user_input <= 7 && user_input >= 1
+  message = "Day #{user_input} of the week is #{days_arr[user_input - 1]}!"
+  #if you use user_input -= 1, then it 'modifies' the user_input, bad practice
+  #don't modify the user inputs, but rather, what exists
 else
-  dow_arr.sample
-  puts "Day #{dow_arr.sample.to_i} of the week is #{dow_arr.sample}!"
-
+  message = "Day #{random_index + 1} of the week is #{random_day}!"
 end
 
-# while dow_arr.include? user_input
+puts message
---
+++ w01/d03/Paul_Lee/Classwork/methods_classwork.rb
@@ -0,0 +1,9 @@
+#method examples: Transmogrifier
+
+def transmogrifier (num_1, num_2,  num_3)
+  result = (num_1 + num_2) ** num_3
+  return result
+end
+
+transmogrified_result = transmogrifier(2, 5, 3)
+puts transmogrified_result
