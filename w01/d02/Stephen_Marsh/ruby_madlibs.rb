21890b27b749b4361117263c13885080a83b18a5
stephenhmarsh <stephenforpresident@gmail.com>
2014-02-25 23:40:33 -0500

    madlibs program fully functional with one template.

--- w01/d02/Stephen_Marsh/ruby_madlibs.rb
+++ w01/d02/Stephen_Marsh/ruby_madlibs.rb
@@ -12,11 +12,24 @@ while playing == "y"
 	puts "Gimme a plural noun:"
 	plural_noun = gets.chomp
 
-	puts "Gimme a"
+	puts "Gimme a color:"
+	color = gets.chomp
+
+	puts "Gimme a number:"
+	number = gets.chomp
+
+	puts "Gimme an adjective:"
+	adjective = gets.chomp
+
+	puts "Gimme a name:"
+	name = gets.chomp
+
+	puts "Gimme a proper noun:"
+	proper_noun = gets.chomp
 
 
 	# Substitute the blank spaces in the template with the words
-
+	template = "Never #{verb} a bear before you go to bed. I did last night and all night there were #{plural_noun} floating around in my head, dancing around like little #{color} Pokemon. One Pokemon, two Pokemon, #{number} Pokemons. All I could think was 'Will I ever be the very best?' I guess I could be as #{adjective} as the greats that came before me. ASH! MISTY! #{name.upcase} ! But alas, only time will tell if I will ever become an amazing #{proper_noun}. But for now, I'll just try to learn Ruby."
 
 	# Print out the results to the user
 	puts template
