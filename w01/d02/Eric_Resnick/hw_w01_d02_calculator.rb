712c832b770c60ff1df25b35a484edfe005fcbb8
eres805 <eres805@gmail.com>
2014-02-25 19:18:42 -0500

    Subtractionn edit

---
+++ w01/d02/Eric_Resnick/hw_w01_d02_calculator.rb
@@ -0,0 +1,20 @@
+#Calculator Homework
+
+puts "Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply"
+math_operation = gets.chomp.downcase
+
+while !((math_operation == 'a') || (math_operation == 's') || (math_operation == 'd') || (math_operation == 'm'))
+	puts "Invalid.  Re-enter with a proper operator"
+	math_operation = gets.chomp.downcase
+end
+
+puts "Enter a value"
+value_1 = gets.chomp.to_i
+
+puts "Enter another value"
+value_2 = gets.chomp.to_i
+
+if math_operation == 'a'
+	puts (value_1 + value_2)
+elsif math_operation == 's'
+	puts (value_1 - value_2)
