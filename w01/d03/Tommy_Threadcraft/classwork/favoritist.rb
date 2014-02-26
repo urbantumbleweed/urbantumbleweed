b207c6238ef690d797710ad46533a2a5becad120
TomSlick08 <tommy.threadcraft@gmail.com>
2014-02-26 16:25:43 -0500

    Updating all my classwork files from the last two days

---
+++ w01/d03/Tommy_Threadcraft/classwork/favoritist.rb
@@ -0,0 +1,20 @@
+# =========================================
+#               FAVORITIST
+# =========================================
+
+
+
+
+#Here's the better code with the method that we defined
+
+def question(category)
+	puts "Please enter your favorite #{category}"
+	gets.chomp
+end
+
+fav_food = question("food")
+fav_color = question("color")
+fav_movie = question("movie")
+fav_song = question("song")
+
+puts "That's awesome! I love #{fav_food}, #{fav_color}, #{fav_movie}, and #{fav_song} too!"
