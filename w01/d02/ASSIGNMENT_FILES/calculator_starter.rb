f1f39b48e97f64f5961d970877cda593cace8830
jingalli <james@endorse.me>
2014-02-26 17:48:56 -0500

    fixed merge issues

--- w01/d02/ASSIGNMENT_FILES/calculator_starter.rb
+++ w01/d02/ASSIGNMENT_FILES/calculator_starter.rb
@@ -1,33 +1,12 @@
 initializer = "start"
 
+# Prompt the user to enter a math operation
+# Prompt the user to enter two values
+# Perform the proper math operation on the two values entered
+# Print out a statement with the result.
 while initializer != "q"
+  print "Press (a) to add, (s) to subtract, (d) to divide, (m) to multiply"
 
-puts "Enter a math operation (+,-,/,*)!"
-op = gets.chomp
 
-puts "Enter first value!"
-x = gets.chomp.to_i
-
-puts "Enter second value!"
-y = gets.chomp.to_i
-
-if op == "+"
-  sum = x + y
-  puts "Your result is #{sum}!"
-elsif op == "-"
-  diff = x - y
-  puts "Your result is #{diff}!"
-elsif op == "*"
-  product = x * y
-  puts "Your result is #{product}!"
-elsif op == "/"
-  divide = x / y
-  puts "Your result is #{divide}!"
+  initializer = gets.chomp
 end
-
-puts "Type q to quit, enter to keep going!"
-
-initializer = gets.chomp
-
-end
-
