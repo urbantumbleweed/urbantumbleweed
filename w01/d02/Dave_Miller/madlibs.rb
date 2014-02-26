92f20d92dbb5381d5246652b69e24107de6d71a2
davidmiller11 <davidmiller11@gmail.com>
2014-02-25 20:18:53 -0500

    madlibs phase 2

--- w01/d02/Dave_Miller/madlibs.rb
+++ w01/d02/Dave_Miller/madlibs.rb
@@ -15,4 +15,6 @@ fillHash( words, :adjective )
 fillHash( words, :name )
 fillHash( words, :proper_noun )
 
+story = "Never #{words[:verb]} a bear before you go to bed. I did last night and all night there were #{words[:noun_plural]} floating around in my head, dancing around like little #{words[:color]} Pokemon. One Pokemon, two Pokemon, #{words[:number]} Pokemons. All I could think was 'Will I ever be the very best?' I guess I could be as #{words[:adjective]} as the greats that came before me. ASH! MISTY! #{words[:name].upcase}! But alas, only time will tell if I will ever become an amazing #{words[:proper_noun]}. But for now, I'll just try to learn Ruby."
 
+# puts story
