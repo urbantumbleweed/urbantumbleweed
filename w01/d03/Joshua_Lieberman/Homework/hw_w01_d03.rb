0452701eac32f91f524978239ac11b42c70c0aa9
jlieb10 <joshuathomaslieberman@gmail.com>
2014-02-26 17:55:00 -0500

    Phase 1 complete, Phase 2 25% -- working with Stephen M.

---
+++ w01/d03/Joshua_Lieberman/Homework/hw_w01_d03.rb
@@ -0,0 +1,8 @@
+n = ['Times Square', '34th', '28th-N', '23rd-N', 'USquare-N', "8th"]
+l = ['8th', '6th', 'USquare-L', '3rd', '1st']
+s = ['gc', '33rd', '28th-6', '23rd-6', 'USquare-6']
+mta = {}
+mta[:n] = n
+mta[:l] = l
+mta[:s] = s
+
