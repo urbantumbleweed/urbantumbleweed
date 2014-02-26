7d0b7ba09d07f15cfd681d154c57bd906d66fa11
eres805 <eres805@gmail.com>
2014-02-25 21:07:59 -0500

    generator edit

--- w01/d02/Eric_Resnick/hw_w01_d02_madlib.rb
+++ w01/d02/Eric_Resnick/hw_w01_d02_madlib.rb
@@ -1,3 +1,5 @@
+#Mad Lib Generator Homework
+
 puts "Welcome to the Mad Lib Generator"
 
 puts "Enter a verb"
@@ -21,3 +23,14 @@ puts "Enter a name"
 puts "Enter a proper noun"
 	noun_prop = gets.chomp.downcase
 
+
+
+puts "Never #{verb} a bear before you go to bed. I did last night 
+and all night there were #{noun} floating around in 
+my head, dancing around like little #{color} Pokemon. 
+One Pokemon, two Pokemon, #{number} Pokemons. All I could 
+think was Will I ever be the very best? I guess I could 
+be as #{adjective} as the greats that came before me. 
+ASH! MISTY! #{name} ! But alas, only time will tell if 
+I will ever become an amazing #{noun_prop}. But for now, 
+I'll just try to learn Ruby."
