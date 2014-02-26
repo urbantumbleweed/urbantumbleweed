8361eb3fa3b36f474c03367461db46a0955b6a3b
eres805 <eres805@gmail.com>
2014-02-25 20:41:56 -0500

    list edit

---
+++ w01/d02/Eric_Resnick/hw_w01_d02_madlib.rb
@@ -0,0 +1,23 @@
+puts "Welcome to the Mad Lib Generator"
+
+puts "Enter a verb"
+	verb = gets.chomp.downcase
+
+puts "Enter a noun (plural)"
+	noun = gets.chomp.downcase
+
+puts "Enter a color"
+	color = gets.chomp.downcase
+
+puts "Enter a number"
+	number = gets.chomp.downcase
+
+puts "Enter an adjective"
+	adjective = gets.chomp.downcase
+
+puts "Enter a name"
+	name = gets.chomp.upcase
+
+puts "Enter a proper noun"
+	noun_prop = gets.chomp.downcase
+
