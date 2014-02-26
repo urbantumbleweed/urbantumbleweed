9260b9ef6e4bf67a78ab1097cff84cc979ab22e9
urbantumbleweeed <saunders.stephen@gmail.com>
2014-02-25 19:11:38 -0500

    Phase 1 completed

---
+++ w01/d02/Stephen_Saunders/madlib.rb
@@ -0,0 +1,31 @@
+
+puts "Welcome to the Mad Lib Generator"
+
+# Prompt the user to enter different words
+puts "Please enter a VERB: "
+verb = gets.chomp
+
+puts "Please enter a PLURAL NOUN: "
+plural_noun = gets.chomp
+
+puts "Ok, now I need a COLOR: "
+color = gets.chomp
+
+puts "Whats the first NUMBER you can think of?"
+number = gets.chomp
+
+puts "Hmm, I need an ADJECTIVE:"
+adjective = gets.chomp
+
+puts "Can you give me a NAME?"
+name = gets.chomp
+
+puts "OK, I just need a PROPER NOUN:"
+proper_noun = gets.chomp.capitalize
+
+# Substitute the blank spaces in the template with the words
+
+
+# Print out the results to the user
+puts template
+
