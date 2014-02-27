41e3f17defc1b171ec811f14dc34c94073a44352
pjwl33 <paul.leejw@gmail.com>
2014-02-27 14:40:24 -0500

    updated w1d3

--- w01/d03/Paul_Lee/Classwork/day_of_week.rb
+++ w01/d03/Paul_Lee/Classwork/day_of_week.rb
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> d8f025f
 #days_arr of week exercise
 
 days_arr = ["Sun", "Mon", "Tue", "Wed"," Thu", "Fri", "Sat"]
@@ -15,32 +11,10 @@ days_arr.unshift(weekend[0])
 days_arr.push(weekend[1])
 
 puts "Our week is back to normal: #{days_arr}"
-<<<<<<< HEAD
-=======
-=======
-#days of week exercise
-
-dow_arr = ["Sun", "Mon", "Tue", "Wed"," Thu", "Fri", "Sat"]
-weekend = [dow_arr.shift, dow_arr.pop]
-
-puts "Weekends are #{weekend}"
-
-puts "Our weekends are gone! #{dow_arr}..sad face"
-
-dow_arr.push("Sat")
-dow_arr.unshift("Sun")
-
-puts "Our week is back to normal: #{dow_arr}"
->>>>>>> backup for w1d3
->>>>>>> d8f025f
 
 puts "Pick a number between 1-7"
 user_input = gets.chomp.to_i
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> d8f025f
 random_day = days_arr.sample
 random_index = days_arr.index(random_day)
 
@@ -53,17 +27,3 @@ else
 end
 
 puts message
-<<<<<<< HEAD
-=======
-=======
-if user_input < dow_arr.length
-  dow_arr[user_input]
-  puts "Day #{user_input} of the week is #{dow_arr[user_input]}!"
-
-else
-  dow_arr.sample
-  puts "Day #{user_input} of the week is #{dow_arr.sample}!"
-
-end
->>>>>>> backup for w1d3
->>>>>>> d8f025f
--- w01/d03/Paul_Lee/Classwork/methods_classwork.rb
+++ w01/d03/Paul_Lee/Classwork/methods_classwork.rb
@@ -4,12 +4,9 @@ def transmogrifier (num_1, num_2,  num_3)
   result = (num_1 + num_2) ** num_3
   return result
 <<<<<<< HEAD
-<<<<<<< HEAD
   puts (result)
 =======
 >>>>>>> origin/master
-=======
->>>>>>> d8f025f
 end
 
 transmogrified_result = transmogrifier(2, 5, 3)
