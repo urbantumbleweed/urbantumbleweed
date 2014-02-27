ff313ff730e87dfec171d6408b35468900d34989
pjwl33 <paul.leejw@gmail.com>
2014-02-27 12:04:17 -0500

    messing up...

--- w01/d03/Paul_Lee/Classwork/day_of_week.rb
+++ w01/d03/Paul_Lee/Classwork/day_of_week.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #days_arr of week exercise
 
 days_arr = ["Sun", "Mon", "Tue", "Wed"," Thu", "Fri", "Sat"]
@@ -11,10 +12,26 @@ days_arr.unshift(weekend[0])
 days_arr.push(weekend[1])
 
 puts "Our week is back to normal: #{days_arr}"
+=======
+#days of week exercise
+
+dow_arr = ["Sun", "Mon", "Tue", "Wed"," Thu", "Fri", "Sat"]
+weekend = [dow_arr.shift, dow_arr.pop]
+
+puts "Weekends are #{weekend}"
+
+puts "Our weekends are gone! #{dow_arr}..sad face"
+
+dow_arr.push("Sat")
+dow_arr.unshift("Sun")
+
+puts "Our week is back to normal: #{dow_arr}"
+>>>>>>> backup for w1d3
 
 puts "Pick a number between 1-7"
 user_input = gets.chomp.to_i
 
+<<<<<<< HEAD
 random_day = days_arr.sample
 random_index = days_arr.index(random_day)
 
@@ -27,3 +44,14 @@ else
 end
 
 puts message
+=======
+if user_input < dow_arr.length
+  dow_arr[user_input]
+  puts "Day #{user_input} of the week is #{dow_arr[user_input]}!"
+
+else
+  dow_arr.sample
+  puts "Day #{user_input} of the week is #{dow_arr.sample}!"
+
+end
+>>>>>>> backup for w1d3
---
+++ w01/d03/Paul_Lee/hw_w01_d03.rb
@@ -0,0 +1,47 @@
+#MTA HW
+#Needs to be refactored better - DRY DRY DYR
+#Does not count for human error in user inputs (e.g. if there was a space added accidently, hash keys/values have to be exact as code)
+
+n = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th']
+l = ['8th ave', '6th ave', 'us', '3rd ave', '1st ave']
+six = ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
+one = ['59th', '50th', 'ts', '34th', '28th-o']
+mta = {}
+mta["n"] = n
+mta["l"] = l
+mta["six"] = six
+mta["one"] = one
+
+#Phase 1 & 2 & 3 & 4 (attempt - could not get multiple transfers to work, but accounted for mutiple transfer points)
+
+puts "Which LINE are you getting ON at (N, L, SIX, or ONE)?"
+line_on = gets.chomp.downcase.to_s
+puts "Which STOP do you want to get ON at? #{mta[line_on]}"
+stop_on = gets.chomp.downcase.to_s
+
+puts "Which LINE are you getting OFF at (N, L, SIX, or ONE)?"
+line_off = gets.chomp.downcase.to_s
+puts "Which STOP do you want to get OFF at? #{mta[line_off]}"
+stop_off = gets.chomp.downcase.to_s
+
+def transfer_calc(subway, first_line, first_stop, sec_line, sec_stop)
+  if subway[first_line].include?('us') && subway[sec_line].include?('us')
+    trip = ( (subway[first_line].index(first_stop) - subway[first_line].index('us') ).abs ) + ( (subway[sec_line].index(sec_stop) - subway[sec_line].index('us') ).abs )
+  elsif subway[first_line].include?('ts') && subway[sec_line].include?('ts')
+    trip = ( (subway[first_line].index(first_stop) - subway[first_line].index('ts') ).abs ) + ( (subway[sec_line].index(sec_stop) - subway[sec_line].index('ts') ).abs )
+  elsif subway[first_line].include?('34th') && subway[sec_line].include?('34th')
+    trip = ( (subway[first_line].index(first_stop) - subway[first_line].index('34th') ).abs ) + ( (subway[sec_line].index(sec_stop) - subway[sec_line].index('34th') ).abs )
+  end
+    puts "Your trip will take #{trip.abs} stops."
+end
+
+if line_on == line_off
+  trip = mta[line_on].index(stop_on) - mta[line_off].index(stop_off)
+  puts "Your trip will take #{trip.abs} stops."
+
+elsif mta.include?(line_on) && mta.include?(line_off)
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
+
+else
+  puts "That line/stop is under construction. Because the MTA is ALWAYS under construction..."
+end
