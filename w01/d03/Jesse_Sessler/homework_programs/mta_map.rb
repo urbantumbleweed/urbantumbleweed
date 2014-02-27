8d23a1a82b38ff971140f39efc4b386cd6055c15
jemise111 <jesse.sessler@gmail.com>
2014-02-26 19:08:31 -0500

    add phase 2 and 3 to mta_map homework

--- w01/d03/Jesse_Sessler/homework_programs/mta_map.rb
+++ w01/d03/Jesse_Sessler/homework_programs/mta_map.rb
@@ -1,31 +1,51 @@
-def subway_menu(line) # takes a subway line array
-  line.each { |l| puts l }
+def subway_menu(line)
+  line.each_with_index { |l, i| puts "#{i.next}. #{l}" }
 end
 
-def num_stops(line, start, last) # takes subway line array, start and last stop strings
+def num_stops(line, start, last)
   (line.index(last) - line.index(start)).abs
 end
 
-def get_stop(line, stop_name)
-  print "#{stop_name} stop: "
-  result = gets.chomp
-  line.include?(result) ? result : get_stop(line, stop_name)
+def num_stops_mult_lines(first_stop_line, first_stop, last_stop_line, last_stop)
+  first_stop_line == last_stop_line ? num_stops(first_stop_line, first_stop, last_stop) :
+  num_stops(first_stop_line, first_stop, 'us') + num_stops(last_stop_line, last_stop, 'us')
 end
 
+def get_and_validate_input(list, prompt)
+  print "#{prompt}: "
+  result = gets.chomp.downcase
+  list.include?(result) ? result : get_and_validate_input(list, prompt)
+end
+
+
 mta = {
-  n: ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'],
-  l: ['8th-l', '6th', 'us', '3rd', '1st'],
-  s: ['gc', '33rd', '28th-s', '23rd-s', 'us']
+  'n' => ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'],
+  'l' => ['8th-l', '6th', 'us', '3rd', '1st'],
+  's' => ['gc', '33rd', '28th-s', '23rd-s', 'us']
 }
 
 # running code
 #===============
 
-puts 'This is the N Line:'
-subway_menu(mta[:n])
-first_stop = get_stop(mta[:n], 'first')
-last_stop = get_stop(mta[:n], 'last')
-puts "That'll be #{num_stops(mta[:n], first_stop, last_stop)} stops."
+# phase 1
+#===============
+# puts 'This is the N Line. Select stops using their names.'
+# subway_menu(mta['n'])
+# first_stop = get_stop(mta['n'], 'first stop')
+# last_stop = get_stop(mta['n'], 'last stop')
+# puts "That'll be #{num_stops(mta['n'], first_stop, last_stop)} stop(s)."
 
+# phase 2 / phase 3
+# ==============
+puts "You're subway line choices are: #{mta.keys.join(', ')}"
+first_stop_line = get_and_validate_input(mta.keys, 'first stop line')
+puts 'The stops on that line are: '
+subway_menu(mta[first_stop_line])
+first_stop = get_and_validate_input(mta[first_stop_line], 'first stop')
 
+last_stop_line = get_and_validate_input(mta.keys, 'last stop line')
+puts 'The stops on that line are: '
+subway_menu(mta[last_stop_line])
+last_stop = get_and_validate_input(mta[last_stop_line], 'last stop')
 
+puts "That'll be #{num_stops_mult_lines(mta[first_stop_line], first_stop, mta[last_stop_line], last_stop)} stop(s)."
