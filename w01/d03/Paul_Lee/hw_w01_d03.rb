0c8acc098906f0c076e72c122b9861c28ef075cb
pjwl33 <paul.leejw@gmail.com>
2014-02-27 00:03:04 -0500

    Completed Assignment, with attemp at Phase 4

--- w01/d03/Paul_Lee/hw_w01_d03.rb
+++ w01/d03/Paul_Lee/hw_w01_d03.rb
@@ -5,24 +5,32 @@
 n = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th']
 l = ['8th ave', '6th ave', 'us', '3rd ave', '1st ave']
 six = ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
+one = ['59th', '50th', 'ts', '34th', '28th-o']
 mta = {}
 mta["n"] = n
 mta["l"] = l
 mta["six"] = six
+mta["one"] = one
 
-#Phase 1 & 2 & 3
-puts "Which LINE are you getting ON at (N or L or SIX)?"
+#Phase 1 & 2 & 3 & 4 (attempt - could not get multiple transfers to work, but accounted for mutiple transfer points)
+puts "Which LINE are you getting ON at (N, L, SIX, or ONE)?"
 line_on = gets.chomp.downcase.to_s
 puts "Which STOP do you want to get ON at? #{mta[line_on]}"
 stop_on = gets.chomp.downcase.to_s
 
-puts "Which LINE are you getting OFF at (N or L or SIX)?"
+puts "Which LINE are you getting OFF at (N, L, SIX, or ONE)?"
 line_off = gets.chomp.downcase.to_s
 puts "Which STOP do you want to get OFF at? #{mta[line_off]}"
 stop_off = gets.chomp.downcase.to_s
 
 def transfer_calc(mta, line_on, stop_on, line_off, stop_off)
-  trip = ( (mta[line_on].index(stop_on) - mta[line_on].index('us') ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index('us') ).abs )
+  if mta[line_on].include?('us') && mta[line_on].include?('us')
+    trip = ( (mta[line_on].index(stop_on) - mta[line_on].index('us') ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index('us') ).abs )
+  elsif mta[line_on].include?('ts') && mta[line_on].include?('ts')
+    trip = ( (mta[line_on].index(stop_on) - mta[line_on].index('ts') ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index('ts') ).abs )
+  elsif mta[line_on].include?('34th') && mta[line_on].include?('34th')
+    trip = ( (mta[line_on].index(stop_on) - mta[line_on].index('34th') ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index('34th') ).abs )
+  end
   puts "Your trip will take #{trip.abs} stops."
 end
 
@@ -34,15 +42,28 @@ if line_on == line_off
   transfer_calc(mta, line_on, stop_on, line_off, stop_off)
   elsif  line_on == "n" &&  line_off == "six"
   transfer_calc(mta, line_on, stop_on, line_off, stop_off)
+  elsif  line_on == "n" &&  line_off == "one"
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
 
   elsif  line_on == "l" &&  line_off == "n"
   transfer_calc(mta, line_on, stop_on, line_off, stop_off)
   elsif  line_on == "l" &&  line_off == "six"
   transfer_calc(mta, line_on, stop_on, line_off, stop_off)
+  elsif  line_on == "l" &&  line_off == "one"
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
 
    elsif  line_on == "six" &&  line_off == "n"
   transfer_calc(mta, line_on, stop_on, line_off, stop_off)
   elsif  line_on == "six" &&  line_off == "l"
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
+  elsif  line_on == "six" &&  line_off == "one"
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
+
+elsif  line_on == "one" &&  line_off == "n"
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
+  elsif  line_on == "one" &&  line_off == "l"
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
+  elsif  line_on == "one" &&  line_off == "six"
   transfer_calc(mta, line_on, stop_on, line_off, stop_off)
 
   else
