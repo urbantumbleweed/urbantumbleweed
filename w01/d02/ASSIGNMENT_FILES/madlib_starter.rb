de10ba39c66f954527871dbd2042b310d01c3c8b
originalblend337 <originalblend337@gmail.com>
2014-02-27 16:48:39 -0500

    fix try1

--- w01/d02/ASSIGNMENT_FILES/madlib_starter.rb
+++ w01/d02/ASSIGNMENT_FILES/madlib_starter.rb
@@ -1,5 +1,6 @@
 
 puts "Welcome to the Mad Lib Generator"
+<<<<<<< HEAD
 
 # Prompt the user to enter different words
 
@@ -9,4 +10,14 @@ puts "Welcome to the Mad Lib Generator"
 
 # Print out the results to the user
 puts template
+=======
 
+# Prompt the user to enter different words
+
+
+# Substitute the blank spaces in the template with the words
+
+>>>>>>> fixed merge issues
+
+# Print out the results to the user
+puts template
