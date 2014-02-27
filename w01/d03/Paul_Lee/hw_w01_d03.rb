d8f025fe5a0bcca90f7a251f256e2069313e4de1
pjwl33 <paul.leejw@gmail.com>
2014-02-27 13:51:27 -0500

    more refactoring, still no go on multiple transfers

--- w01/d03/Paul_Lee/hw_w01_d03.rb
+++ w01/d03/Paul_Lee/hw_w01_d03.rb
@@ -2,10 +2,10 @@
 #Needs to be refactored better - DRY DRY DYR
 #Does not count for human error in user inputs (e.g. if there was a space added accidently, hash keys/values have to be exact as code)
 
-n = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th']
+n = ['ts', '34th-n', '28th-n', '23rd-n', 'us', '8th']
 l = ['8th ave', '6th ave', 'us', '3rd ave', '1st ave']
 six = ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
-one = ['59th', '50th', 'ts', '34th', '28th-o']
+one = ['59th', '50th', 'ts', '34th-o', '28th-o']
 mta = {}
 mta["n"] = n
 mta["l"] = l
@@ -26,18 +26,25 @@ stop_off = gets.chomp.downcase.to_s
 
 def transfer_calc(subway, first_line, first_stop, sec_line, sec_stop)
   if subway[first_line].include?('us') && subway[sec_line].include?('us')
-    trip = ( (subway[first_line].index(first_stop) - subway[first_line].index('us') ).abs ) + ( (subway[sec_line].index(sec_stop) - subway[sec_line].index('us') ).abs )
+    us_transfer = ( (subway[first_line].index(first_stop) - subway[first_line].index('us') ).abs ) + ( (subway[sec_line].index(sec_stop) - subway[sec_line].index('us') ).abs )
+
+    puts "Your trip will take #{us_transfer.abs} stops."
+
   elsif subway[first_line].include?('ts') && subway[sec_line].include?('ts')
-    trip = ( (subway[first_line].index(first_stop) - subway[first_line].index('ts') ).abs ) + ( (subway[sec_line].index(sec_stop) - subway[sec_line].index('ts') ).abs )
-  elsif subway[first_line].include?('34th') && subway[sec_line].include?('34th')
-    trip = ( (subway[first_line].index(first_stop) - subway[first_line].index('34th') ).abs ) + ( (subway[sec_line].index(sec_stop) - subway[sec_line].index('34th') ).abs )
+    ts_transfer = ( (subway[first_line].index(first_stop) - subway[first_line].index('ts') ).abs ) + ( (subway[sec_line].index(sec_stop) - subway[sec_line].index('ts') ).abs )
+
+    puts "Your trip will take #{ts_transfer.abs} stops."
+
+  # elsif  first_line == 'one' || sec_line == 'one'
+  #   multi_transfer = us_transfer + ts_transfer
+  #   puts "Your trip will take #{multi_transfer.abs} stops."
   end
-    puts "Your trip will take #{trip.abs} stops."
+
 end
 
 if line_on == line_off
-  trip = mta[line_on].index(stop_on) - mta[line_off].index(stop_off)
-  puts "Your trip will take #{trip.abs} stops."
+  single_line_trip = mta[line_on].index(stop_on) - mta[line_off].index(stop_off)
+  puts "Your trip will take #{single_line_trip.abs} stops."
 
 elsif mta.include?(line_on) && mta.include?(line_off)
   transfer_calc(mta, line_on, stop_on, line_off, stop_off)
