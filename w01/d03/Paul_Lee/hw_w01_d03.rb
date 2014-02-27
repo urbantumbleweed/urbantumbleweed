48d5c53af052c9c85501c4bb38b5e184d958153b
pjwl33 <paul.leejw@gmail.com>
2014-02-26 23:21:51 -0500

    refactored

--- w01/d03/Paul_Lee/hw_w01_d03.rb
+++ w01/d03/Paul_Lee/hw_w01_d03.rb
@@ -1,10 +1,10 @@
 #MTA HW
 #Needs to be refactored better - DRY DRY DYR
-#Does not count for human error in user inputs (e.g. if there was a space added accidently)
+#Does not count for human error in user inputs (e.g. if there was a space added accidently, hash keys/values have to be exact as code)
 
-n = ["TS", "34", "28-N", "23-N", "US-N", "8-N"]
-l = ["8 AVE", "6 AVE", "US-L", "3 AVE", "1 AVE"]
-six = ["GC", "33", "28-S", "23-S", "US-S", "AP"]
+n = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th']
+l = ['8th ave', '6th ave', 'us', '3rd ave', '1st ave']
+six = ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
 mta = {}
 mta["n"] = n
 mta["l"] = l
@@ -14,33 +14,39 @@ mta["six"] = six
 puts "Which LINE are you getting ON at (N or L or SIX)?"
 line_on = gets.chomp.downcase.to_s
 puts "Which STOP do you want to get ON at? #{mta[line_on]}"
-stop_on = gets.chomp.upcase.to_s
+stop_on = gets.chomp.downcase.to_s
 
 puts "Which LINE are you getting OFF at (N or L or SIX)?"
 line_off = gets.chomp.downcase.to_s
 puts "Which STOP do you want to get OFF at? #{mta[line_off]}"
-stop_off = gets.chomp.upcase.to_s
+stop_off = gets.chomp.downcase.to_s
+
+def transfer_calc(mta, line_on, stop_on, line_off, stop_off)
+  trip = ( (mta[line_on].index(stop_on) - mta[line_on].index('us') ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index('us') ).abs )
+  puts "Your trip will take #{trip.abs} stops."
+end
 
 if line_on == line_off
   trip = mta[line_on].index(stop_on) - mta[line_off].index(stop_off)
+  puts "Your trip will take #{trip.abs} stops."
 
   elsif  line_on == "n" &&  line_off == "l"
-  trip = ( (mta[line_on].index(stop_on) - mta[line_on].index("US-N") ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index("US-L") ).abs )
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
   elsif  line_on == "n" &&  line_off == "six"
-  trip = ( (mta[line_on].index(stop_on) - mta[line_on].index("US-N") ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index("US-S") ).abs )
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
 
   elsif  line_on == "l" &&  line_off == "n"
-  trip = ( (mta[line_on].index(stop_on) - mta[line_on].index("US-L") ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index("US-N") ).abs )
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
   elsif  line_on == "l" &&  line_off == "six"
-  trip = ( (mta[line_on].index(stop_on) - mta[line_on].index("US-L") ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index("US-S") ).abs )
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
 
    elsif  line_on == "six" &&  line_off == "n"
-  trip = ( (mta[line_on].index(stop_on) - mta[line_on].index("US-S") ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index("US-N") ).abs )
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
   elsif  line_on == "six" &&  line_off == "l"
-  trip = ( (mta[line_on].index(stop_on) - mta[line_on].index("US-S") ).abs ) + ( (mta[line_off].index(stop_off) - mta[line_off].index("US-L") ).abs )
+  transfer_calc(mta, line_on, stop_on, line_off, stop_off)
 
   else
   puts "Sorry, that line is under construction"
 end
 
-puts "Your trip will take #{trip.abs} stops."
+
