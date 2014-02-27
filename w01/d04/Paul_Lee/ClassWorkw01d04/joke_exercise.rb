d1fdb0e07ec2f521d03079f450cc726be9f2d80e
pjwl33 <paul.leejw@gmail.com>
2014-02-27 15:31:28 -0500

    joke exercise finished

--- w01/d04/Paul_Lee/ClassWorkw01d04/joke_exercise.rb
+++ w01/d04/Paul_Lee/ClassWorkw01d04/joke_exercise.rb
@@ -34,25 +34,25 @@ joke2()
 # 3. Modify the method so that you can give it an arbitrary joke, which it will show to the user instead of the original joke.
 #   * (2 lines)
 print ' > #3 '
-def joke_teller(joke)
+def joke3(joke)
   puts joke
   return "joke told!"
 end
 
-joke_teller("Dog are always in the push-up position")
-joke_teller("Your mama's so fat...")
+joke3("Dogs are always in the push-up position")
+joke3("Your mama's so fat...")
 
 
 # 4. Modify the method so that, after displaying the joke, it takes user input from the terminal and stores it in a variable.
 #   * (Continue to return 'joke told!').
 print ' > #4 '
-def joke_teller(joke)
+def joke4(joke)
   puts joke
   return "joke told!"
 end
 
-joke_teller("Dog are always in the push-up position")
-joke_teller("Your mama's so fat...")
+joke4("Dogs are always in the push-up position")
+joke4("Your mama's so fat...")
 
 puts "Do you get it?"
 
@@ -61,14 +61,14 @@ puts "Do you get it?"
 
 # 5. Modify the method so that it returns the user input.
 print ' > #5 '
-def joke_teller(joke)
+def joke5(joke)
   puts joke
   user_input = gets.chomp
   return user_input
 end
 
-joke_teller("Dog are always in the push-up position")
-joke_teller("Your mama's so fat...")
+joke5("Dogs are always in the push-up position")
+joke5("Your mama's so fat...")
 
 puts "Do you get it?"
 
@@ -78,7 +78,7 @@ puts "Do you get it?"
 # 6. Modify the method so that if the user input is "I don't know" it displays a snarky comment, otherwise, it says "you got my joke!"
 #   * continue to return the user input
 print ' > #6 '
-def joke_teller(joke)
+def joke6(joke)
   puts joke
   user_input = gets.chomp
   if user_input == "I don't know"
@@ -89,8 +89,8 @@ def joke_teller(joke)
   return user_input
 end
 
-joke_teller("Dog are always in the push-up position")
-joke_teller("Your mama's so fat...")
+joke6("Dogs are always in the push-up position")
+joke6("Your mama's so fat...")
 
 puts "Do you get it?"
 
@@ -101,7 +101,7 @@ puts "Do you get it?"
 # 7. Modify the method so that it returns true or false. It should return false if the user said 'I don't know", true otherwise.
 #   * continue to display snarky comment, "you got my joke"
 print ' > #7 '
-def joke_teller(joke)
+def joke7(joke)
   puts joke
   user_input = gets.chomp
   if user_input == "I don't know"
@@ -114,7 +114,7 @@ def joke_teller(joke)
 end
 
 hilarious_joke = "Dogs are always in the push-up position"
-joke_result = joke_teller(hilarious_joke)
+joke_result = joke7(hilarious_joke)
 
 if joke_result == true
   puts "great job!"
@@ -136,7 +136,7 @@ def joke8(joke, correct_response)
 end
 
 hilarious_joke = "Dogs are always in the push-up position"
-joke_result = joke_teller(hilarious_joke, "Response")
+joke_result = joke8(hilarious_joke, "You can't tuna fish...")
 
 if joke_result == true
   puts "great job!"
@@ -147,6 +147,30 @@ end
 puts "Do you get it?"
 print ' > END '
 
+#### Exercise 2
+
+# 1. Write a new method called "tell_two_jokes". It should call our original method, and take 4 arguments, joke1, answer1, joke2, answer2.
+def joke_ex2(joke, correct_response)
+  puts joke
+  user_input = gets.chomp
+  if user_input ==  correct_response
+    return true
+  else
+   return false
+  end
+end
+
+def tell_two_jokes(joke1, correct_asn1, joke2, correct_ans2)
+  result1 = joke_ex2(joke1, correct_asn1)
+  result2 = joke_ex2(joke2, correct_ans2)
+  if  result1  && result2
+    return true
+  else
+    return false
+  end
+end
+
+tell_two_jokes("asss?", "yessss", "poooop?", "noooooo")
 
 
 
