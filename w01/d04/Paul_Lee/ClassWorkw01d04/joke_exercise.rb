c7a83583dddac7e4f2e312b2fe80d80032f8b38a
pjwl33 <paul.leejw@gmail.com>
2014-02-27 13:50:19 -0500

    joke exercise

--- w01/d04/Paul_Lee/ClassWorkw01d04/joke_exercise.rb
+++ w01/d04/Paul_Lee/ClassWorkw01d04/joke_exercise.rb
@@ -16,6 +16,7 @@ end
 
 joke1()
 joke1()
+print '>'
 
 # 2. Modify the method so that it still displays the joke, but this time returns the string "joke told!" as well.
 #   * (2 lines)
@@ -26,6 +27,7 @@ end
 
 joke2()
 joke2()
+print '>'
 
 # 3. Modify the method so that you can give it an arbitrary joke, which it will show to the user instead of the original joke.
 #   * (2 lines)
@@ -38,6 +40,7 @@ end
 
 joke_teller("Dog are always in the push-up position")
 joke_teller("Your mama's so fat...")
+print '>'
 
 # 4. Modify the method so that, after displaying the joke, it takes user input from the terminal and stores it in a variable.
 #   * (Continue to return 'joke told!').
@@ -49,6 +52,92 @@ end
 
 joke_changer("Dog are always in the push-up position")
 joke_changer("Your mama's so fat...")
+
+puts "Do you get it?"
+user_input = gets.chomp
+print '>'
+
+# ** Paws **
+
+# 5. Modify the method so that it returns the user input.
+
+def joke_changer(joke, input)
+  puts joke
+  return input
+end
+
+joke_changer("Dog are always in the push-up position", user_input)
+joke_changer("Your mama's so fat...", user_input)
+
+puts "Do you get it?"
+user_input = gets.chomp
+print '>'
+
+
+# 6. Modify the method so that if the user input is "I don't know" it displays a snarky comment, otherwise, it says "you got my joke!"
+#   * continue to return the user input
+
+def joke_changer(joke, input)
+  puts joke
+  if input == "I don't know"
+    puts "Snarky comment."
+  else
+    puts "You got my joke!"
+  end
+  return input
+end
+
+joke_changer("Dog are always in the push-up position", user_input)
+joke_changer("Your mama's so fat...", user_input)
+
+puts "Do you get it?"
+user_input = gets.chomp
+print '>'
+
+# ** Paws **
+
+# 7. Modify the method so that it returns true or false. It should return false if the user said 'I don't know", true otherwise.
+#   * continue to display snarky comment, "you got my joke"
+
+def joke_changer(joke, input)
+  puts joke
+  if input == "I don't know"
+    puts "Snarky comment."
+    return false
+  else
+    puts "You got my joke!"
+    return true
+  end
+  return input
+end
+
+joke_changer("Dog are always in the push-up position", user_input)
+joke_changer("Your mama's so fat...", user_input)
+
+puts "Do you get it?"
+user_input = gets.chomp
+print '>'
+
+# 8. Modify the method such that you can give it a 'correct response' to expect from the user. The method should behave as above, only now return true if the input matches the correct response, false otherwise.
+
+def joke_changer(joke, input)
+  puts joke
+  if input == "Correct Response"
+    puts "You got my joke!"
+    return true
+  else
+    puts "Snarky response"
+    return false
+  end
+  return input
+end
+
+joke_changer("Dog are always in the push-up position", user_input)
+joke_changer("Your mama's so fat...", user_input)
+
+puts "Do you get it?"
+user_input = gets.chomp
+print '>'
 
 
 
