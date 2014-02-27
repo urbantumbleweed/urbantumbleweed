24b6b273c6947824fdd46b78aa2368c94c1f97e1
verner-dz <verner.dz@gmail.com>
2014-02-26 22:50:27 -0500

    finished phase 3

--- w01/d03/Verner_DSouza/mta_app.rb
+++ w01/d03/Verner_DSouza/mta_app.rb
@@ -29,22 +29,25 @@
 # #####Phase 2
 
 # - Two lines functionality (N and L)
-puts "Welcome to Line N and L"
+puts "Welcome to Line N and L and S"
 
 #   - The user should be asked what line they want to get on
-puts "What line do you want to get on? Select N or L"
+puts "What line do you want to get on? Select N or L or S"
 gets_on_line = gets.chomp.downcase
 
 #   - The user should be given a list of all the stops on that line
 n = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"]
 l = ["8th Ave", "6th Ave", "Union Square", "3rd Ave", "1st Ave"]
-mta = {"n" => n , "l" => l}
+s = ["gc", "33rd", "28th-s", "23rd-s", "Union Square", "Astor"]
+mta = {"n" => n , "l" => l, "s" => s}
 if gets_on_line =="n"
   puts n
 elsif gets_on_line == "l"
   puts l
+elsif gets_on_line == "s"
+  puts s
 else
-  puts "Please select N or L"
+  puts "Please select n or l or s"
 end
 
 #   - The user should be able to enter the stop that they want to get on at
@@ -60,9 +63,11 @@ if gets_off_line == "n"
     puts n
   elsif gets_off_line == "l"
     puts l
+  elsif gets_off_line == "s"
+    puts s
   else
-    puts "Please select n or l"
-  end
+    puts "Please select n or l or s"
+end
 
 #   - The user should be able to enter the stop that they want to get off at
  puts "What stop do you want to get off at?"
@@ -77,7 +82,7 @@ def index_of_union_square(line_id, mta)
 end
 
 #   - The user should be told the number of stops for their trip
-  puts mta[gets_on_line].index(gets_on_stop)
+
    if gets_off_line == gets_on_line
    number_of_stops = mta[gets_off_line].index(gets_off_stop) - mta[gets_on_line].index(gets_on_stop)
    final_stop = number_of_stops.abs
@@ -88,6 +93,7 @@ end
    number_of_stops = (mta[gets_off_line].index(gets_off_stop) - index_of_union_square(gets_off_line, mta)) + (index_of_union_square(gets_on_line, mta) - mta[gets_on_line].index(gets_on_stop))
    final_stop = number_of_stops.abs
     puts "You have #{final_stop} stops left!"
+
   end
 
 
