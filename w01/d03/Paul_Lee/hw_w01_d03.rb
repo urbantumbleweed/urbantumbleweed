e41e785215c44815295a8efc32a50455bd877995
pjwl33 <paul.leejw@gmail.com>
2014-02-26 19:06:54 -0500

    phase 2 completed, no transfer accounted for yet

--- w01/d03/Paul_Lee/hw_w01_d03.rb
+++ w01/d03/Paul_Lee/hw_w01_d03.rb
@@ -1,17 +1,28 @@
 #MTA HW
-#Phase 1
 
-n_line = ["TS", "34", "28-N", "23", "US-N", "8"]
+mta = { "n" => ["TS", "34", "28-N", "23-N", "US-N", "8"], "l" => ["8", "6", "US-L", "3", "1"], "6" => ["GC", "33", "28-6", "23-6", "US-6", "AP"] }
 
-puts "You're getting on the (N) line - where do you want to get on #{n_line}?"
+puts "What line are you getting on? The n, l , or 6 (all others are under construction)?"
+user_line = gets.chomp.to_s
 
-first_stop = gets.chomp.to_s
-if n_line.include? first_stop
-  puts "Where would you like to get off #{n_line}?"
-  sec_stop = gets.chomp.to_s
-  if n_line.include? sec_stop
-    trip = n_line.index(sec_stop) - n_line.index(first_stop)
-    puts "Your trip will take #{trip.abs} stops."
-    #.abs = absolulte value
+def trip_calculator(line, mta)
+  puts "You're getting on the #{line} line - where do you want to get on #{mta[line]}?"
+  first_stop = gets.chomp.upcase.to_s
+  if mta[line].include? first_stop
+    puts "Where would you like to get off #{mta}?"
+    sec_stop = gets.chomp.upcase.to_s
+
+#Check for Transfer
+
+    if mta[line].include? sec_stop
+      trip = mta[line].index(sec_stop) - mta[line].index(first_stop)
+      puts "Your trip will take #{trip.abs} stops."
+      #.abs = absolulte value
+    end
   end
 end
+
+trip_calculator(user_line, mta)
+
+
+
