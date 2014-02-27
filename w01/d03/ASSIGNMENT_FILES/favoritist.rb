9010ba5bac6ab166091798503f9d0098d6007da0
McKenneth Scott III <mckenneth.scott@icloud.com>
2014-02-27 11:35:01 -0500

    fixes edit to assignment files

--- w01/d03/ASSIGNMENT_FILES/favoritist.rb
+++ w01/d03/ASSIGNMENT_FILES/favoritist.rb
@@ -1,17 +1,17 @@
 # =========================================
 #               FAVORITIST
 # =========================================
-def fave(food, color, movie, song)
-	h = {"food" => 100, "color" => 200, "movie" => 200, "song" => 200}
 
-while key.length < 4
-	h.each {|key, value| puts "whats your fave #{key}?"}
-	gets.chomp
+puts("Please enter your favorite food:")
+fav_food = gets.chomp
 
-}
-	puts("That's awesome! I love #{h["food"]}, #{h["color"]}, #{h["movie"]}, and #{h["song"]}, too!")
-end
-end
+puts("Please enter your favorite color:")
+fav_color = gets.chomp
 
-tale = fave(food, color, movie, song)
-puts tale
+puts("Please enter your favorite movie:")
+fav_movie = gets.chomp
+
+puts("Please enter your favorite song:")
+fav_song = gets.chomp
+
+puts("That's awesome! I love #{fav_food}, #{fav_food}, #{fav_food}, and #{fav_food}, too!")
