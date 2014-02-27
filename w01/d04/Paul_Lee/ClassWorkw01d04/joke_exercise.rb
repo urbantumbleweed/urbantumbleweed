7660454580dadf2e675872020500272fd27aef7f
pjwl33 <paul.leejw@gmail.com>
2014-02-27 14:39:43 -0500

    joke exercise update

--- w01/d04/Paul_Lee/ClassWorkw01d04/joke_exercise.rb
+++ w01/d04/Paul_Lee/ClassWorkw01d04/joke_exercise.rb
@@ -10,16 +10,18 @@
 
 # 1. Write a method that displays a joke to the user (you pick the joke).
 #   * (This method should have 1 line in the body.)
+print ' > #1 '
 def joke1
   puts "Why did the chicken cross the road? To get to the other side."
 end
 
 joke1()
 joke1()
-print '>'
+
 
 # 2. Modify the method so that it still displays the joke, but this time returns the string "joke told!" as well.
 #   * (2 lines)
+print ' > #2 '
 def joke2
   puts "Why did the chicken cross the road? To get to the other side."
   return "joke told!"
@@ -27,12 +29,11 @@ end
 
 joke2()
 joke2()
-print '>'
+
 
 # 3. Modify the method so that you can give it an arbitrary joke, which it will show to the user instead of the original joke.
 #   * (2 lines)
-
-
+print ' > #3 '
 def joke_teller(joke)
   puts joke
   return "joke told!"
@@ -40,104 +41,113 @@ end
 
 joke_teller("Dog are always in the push-up position")
 joke_teller("Your mama's so fat...")
-print '>'
+
 
 # 4. Modify the method so that, after displaying the joke, it takes user input from the terminal and stores it in a variable.
 #   * (Continue to return 'joke told!').
-
-def joke_changer(joke)
+print ' > #4 '
+def joke_teller(joke)
   puts joke
   return "joke told!"
 end
 
-joke_changer("Dog are always in the push-up position")
-joke_changer("Your mama's so fat...")
+joke_teller("Dog are always in the push-up position")
+joke_teller("Your mama's so fat...")
 
 puts "Do you get it?"
-user_input = gets.chomp
-print '>'
+
 
 # ** Paws **
 
 # 5. Modify the method so that it returns the user input.
-
-def joke_changer(joke, input)
+print ' > #5 '
+def joke_teller(joke)
   puts joke
-  return input
+  user_input = gets.chomp
+  return user_input
 end
 
-joke_changer("Dog are always in the push-up position", user_input)
-joke_changer("Your mama's so fat...", user_input)
+joke_teller("Dog are always in the push-up position")
+joke_teller("Your mama's so fat...")
 
 puts "Do you get it?"
-user_input = gets.chomp
-print '>'
+
+
 
 
 # 6. Modify the method so that if the user input is "I don't know" it displays a snarky comment, otherwise, it says "you got my joke!"
 #   * continue to return the user input
-
-def joke_changer(joke, input)
+print ' > #6 '
+def joke_teller(joke)
   puts joke
-  if input == "I don't know"
+  user_input = gets.chomp
+  if user_input == "I don't know"
     puts "Snarky comment."
   else
     puts "You got my joke!"
   end
-  return input
+  return user_input
 end
 
-joke_changer("Dog are always in the push-up position", user_input)
-joke_changer("Your mama's so fat...", user_input)
+joke_teller("Dog are always in the push-up position")
+joke_teller("Your mama's so fat...")
 
 puts "Do you get it?"
-user_input = gets.chomp
-print '>'
+
+
 
 # ** Paws **
 
 # 7. Modify the method so that it returns true or false. It should return false if the user said 'I don't know", true otherwise.
 #   * continue to display snarky comment, "you got my joke"
-
-def joke_changer(joke, input)
+print ' > #7 '
+def joke_teller(joke)
   puts joke
-  if input == "I don't know"
+  user_input = gets.chomp
+  if user_input == "I don't know"
     puts "Snarky comment."
     return false
   else
     puts "You got my joke!"
     return true
   end
-  return input
 end
 
-joke_changer("Dog are always in the push-up position", user_input)
-joke_changer("Your mama's so fat...", user_input)
+hilarious_joke = "Dogs are always in the push-up position"
+joke_result = joke_teller(hilarious_joke)
 
-puts "Do you get it?"
-user_input = gets.chomp
-print '>'
+if joke_result == true
+  puts "great job!"
+else
+  puts "maybe next time"
+end
+
 
 # 8. Modify the method such that you can give it a 'correct response' to expect from the user. The method should behave as above, only now return true if the input matches the correct response, false otherwise.
-
-def joke_changer(joke, input)
+print ' > #8 '
+def joke_teller(joke)
   puts joke
-  if input == "Correct Response"
+  user_input = gets.chomp
+  if user_input == "I don't know"
+    puts "Snarky comment."
+    return false
+  else
     puts "You got my joke!"
     return true
-  else
-    puts "Snarky response"
-    return false
   end
-  return input
 end
 
-joke_changer("Dog are always in the push-up position", user_input)
-joke_changer("Your mama's so fat...", user_input)
+hilarious_joke = "Dogs are always in the push-up position"
+joke_result = joke_teller(hilarious_joke)
+
+if joke_result == true
+  puts "great job!"
+else
+  puts "maybe next time"
+end
 
 puts "Do you get it?"
-user_input = gets.chomp
-print '>'
+print ' > END '
 
 
 
