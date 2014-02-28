d58abf624b3afa4e727649671049c47c72ae45b0
Ash Kamel <ajkamel@gmail.com>
2014-02-27 20:51:33 -0500

    Add middle number method on Part 4

--- w01/d04/Ash_Kamel/homework/moar_methods.rb
+++ w01/d04/Ash_Kamel/homework/moar_methods.rb
@@ -1,10 +1,36 @@
+#Excercise 3
+
+# num_array = [1, 3, 5, 6]
+
+# def sw_array(array)
+
+# 	if array.length % 2 == 0
+
+# 		return puts "There is no middle number"
+# 	else
+
+# 		array[array.length/2]
+
+# 	end	
+
+# end
+
+# puts sw_array(num_array)
+
+
+#Excercise 4
 num_array = [1, 3, 5, 6]
 
 def sw_array(array)
 
-	array.pop
+	if array.length % 2 == 0
 
-	return 
+		return puts "There is no middle number"
+	else
+
+		array[array.length/2]
+
+	end	
 
 end
 
