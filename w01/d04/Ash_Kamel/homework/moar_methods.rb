4f7eed08beaeedd51498bdc73b6bc18a73808598
Ash Kamel <ajkamel@gmail.com>
2014-02-27 21:34:55 -0500

    Add second element into array using third element as index

--- w01/d04/Ash_Kamel/homework/moar_methods.rb
+++ w01/d04/Ash_Kamel/homework/moar_methods.rb
@@ -18,20 +18,43 @@
 # puts sw_array(num_array)
 
 
-#Excercise 4
+# #Excercise 4
+# num_array = [1, 3, 5, 6, 8]
+# wild_card = 10
+
+# def sw_array(array, wd)
+
+# 	if array.length % 2 == 0
+
+# 		return nil
+# 	else
+
+# 	middle_index = array.length/2
+	
+# 	array[middle_index] = wd
+
+# 	return puts array
+
+# 	end	
+
+# end
+
+# puts sw_array(num_array, wild_card)
+
+
+#Excercise 5
 num_array = [1, 3, 5, 6, 8]
 wild_card = 10
+wild_card2 = 11
 
-def sw_array(array, wd)
+def sw_array(array, wd, index)
 
 	if array.length % 2 == 0
 
 		return nil
 	else
 
-	middle_index = array.length/2
-	
-	array[middle_index] = wd
+	array[index] = wd
 
 	return puts array
 
@@ -39,4 +62,4 @@ def sw_array(array, wd)
 
 end
 
-puts sw_array(num_array, wild_card)
+puts sw_array(num_array, wild_card, wild_card2)
