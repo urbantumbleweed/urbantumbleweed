1e13140286256cb7eb7e31d63101afe3ee855210
Adam Bray <adam.bray@yale.edu>
2014-02-28 16:50:23 -0500

    mail

--- w01/d05/INSTRUCTOR/mailman/postman.rb
+++ w01/d05/INSTRUCTOR/mailman/postman.rb
@@ -1,14 +1,18 @@
 class Postman
+  attr_reader :letters
+
   def initialize(letters)
     @letters = letters
   end
 
-  def letters
-    @letters
-  end
-
   def deliver(mailbox)
-    mailbox.receive(letters[mailbox.address])
-    letters[mailbox.address].clear
+    letters_for_address = @letters[mailbox.address]
+    if letters_for_address
+      puts("delivering to mailbox at address #{mailbox.address}")
+      mailbox.receive(@letters[mailbox.address])
+      @letters[mailbox.address].clear
+    else
+      puts("no letters today for #{mailbox.address}")
+    end
   end
 end
