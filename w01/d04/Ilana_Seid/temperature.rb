25e3a11dec14b501a613f19b0d5d2bb56d7f0ac0
ilanaseid <ilanaseid@gmail.com>
2014-02-27 22:23:33 -0500

    temp

--- w01/d04/Ilana_Seid/temperature.rb
+++ w01/d04/Ilana_Seid/temperature.rb
@@ -1,27 +1,4 @@
-# def converter(from_degree, degree_to_convert, to_degree)
-# 	puts "From degree do you want to convert from? C = celsius, K = kelvin, f = fahrenheit."
-# 	 = gets.chomp.to_i
-	
-# 	puts "Do you want to conver to kelvin (k) or fahrenheit (f)?"
-# 	k_or_f = gets.chomp.downcase
 
-# 	if k_or_f == "k"
-# 		temp_output = temp_celsius + 273.15
-# 	else
-# 		temp_output = (temp_celsius * 1.8) + 32.00
-# 	end
-
-# 	if k_or_f == "k"
-# 		end_unit = "kelvin"
-# 	else
-# 		end_unit = "fahrenheit"
-# 	end
-
-# puts "thats #{temp_output} in #{end_unit}."
-
-# end
-
-# convert_to_fahrenheit(4)
 
 def converter(degree_celsius)
 	puts "Enter temperature in degrees celsius: "
@@ -46,4 +23,19 @@ puts "#{degree_celsius} degrees celsius is #{temp_output} in #{end_unit}."
 
 end
 
-converter(4)
+# converter(4)
+puts "From what measurement are you converting from? kelvin (k) or fahrenheit (f) or celsius (c)?"
+unit_in = gets.chomp.downcase
+
+puts "To what measurement would you like to convert to? kelvin (k) or fahrenheit (f) or celsius (c)?"
+unit_out = gets.chomp.downcase
+
+puts "Enter degree: "
+number_in = gets.chomp
+
+def temp_convert(unit_in, unit_out, number_in)
+	if unit_in == unit_out
+		return number_in
+	elsif 
+
+end
