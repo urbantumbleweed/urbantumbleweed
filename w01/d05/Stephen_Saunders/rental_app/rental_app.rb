e7a83f7a19cf866462ef56f62df1c501db501930
urbantumbleweeed <saunders.stephen@gmail.com>
2014-03-01 00:16:21 -0500

    case statement switch statement, and created add_new_apartment

--- w01/d05/Stephen_Saunders/rental_app/rental_app.rb
+++ w01/d05/Stephen_Saunders/rental_app/rental_app.rb
@@ -2,14 +2,88 @@ require_relative("apartment")
 require_relative("building")
 require_relative("person")
 
+
+
+def final_msg(initializer)
+	puts ""
+	puts "Would you like help with something else? y/n"
+	print "> "
+
+	initializer = gets.chomp
+	initializer == "y" ? initializer = "go_again" : initializer = "q"
+	return initializer
+end
+
+def add_new_apartment(initializer)
+	puts "Ok, let's add an appartment to the building:"
+	puts "First I'll need an apartment number"
+	print "> "
+	apt_num = gets.chomp
+
+	puts "Next, I'll need the price of the rent:"
+	print "> "
+	rent = gets.chomp.to_i
+
+	puts "Ok, now tell me the size of the apartment in square feet:"
+	print "> "
+	size = gets.chomp.to_i
+
+	puts "How many bedrooms are there?"
+	print "> "
+	bedrooms = gets.chomp.to_i
+
+	puts "How many bathrooms are there?"
+	print "> "
+	bathrooms = gets.chomp.to_i
+	
+	# penthouse = Apartment.new(1200, 3100, 795, 2, 1)
+	new_apartment = Apartment.new(apt_num, rent, size, bedrooms, bathrooms)
+
+	puts "You have added apartment #{apt_num}. It is #{size}sqft, has #{bedrooms} bedrooms and #{bathrooms} bathrooms.  The rent is $#{rent}/month."
+	final_msg(initializer)
+
+end
+
 puts "||         ||   ||\n" * 1
 puts "||         ||"
 puts "||         ||   ||\n" * 1
 puts "|||||||||||||   ||"
 puts "||         ||   ||\n" * 3
 puts  "\n" * 3
+puts "Welcome to this nifty rental app."
 
-puts "hello"
+initializer = "menu"
+puts initializer #for debug
+while initializer != "q"
+
+if initializer != "q"
+	puts "Would you like to do?" 
+	puts "(enter letter)"
+	puts "[a] Add an APARTMENT to the building"
+	puts "[t] Add a TENANT to an apartment"
+	puts "[l] LIST the apartment directory"
+	#puts "[s] See the available apartments in the building"
+	#puts "[r] See renters in apartments"
+	puts "[q] QUIT the program"
+	print "> "
+	initializer = gets.chomp
+end
+
+
+case
+when initializer == "a"
+	initializer = add_new_apartment(initializer)
+when initializer == "t"
+
+when initializer == "l"
+
+when initializer == "q"
+	puts "Have a good one.  I hope I could help."
+
+end
+
+# initializer = "q"
+end
 
 
 
