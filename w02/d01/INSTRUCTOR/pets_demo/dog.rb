8693ed096fb8d1cfc23afa300372a0b5f3032b5c
harimohanraj89 <hari.mohanraj89@gmail.com>
2014-03-03 09:28:56 -0500

    pet inheritance example added

---
+++ w02/d01/INSTRUCTOR/pets_demo/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  attr_accessor(:name, :color, :ear_type)
+  attr_reader(:friends)
+
+  def initialize(name, color, ear_type)
+    @name = name
+    @color = color
+    @ear_type = ear_type
+    @friends = []
+  end
+
+  def bark
+    puts("WOOF WOOF")
+  end
+
+  def add_friend(new_animal)
+    @friends << new_animal
+  end
+end
